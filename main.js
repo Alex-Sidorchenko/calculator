25e7899dcd67351e5df13e1e3541cfdc782358dc
Александр <36203579+Alex-Sidorchenko@users.noreply.github.com>
2018-03-30 16:37:45 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function plus() {
+    document.getElementById('inp').value+='+';
+}
+
+function minus() {
+    document.getElementById('inp').value+='-';
+}
+
+function multiply() {
+    document.getElementById('inp').value+='x';
+}
+
+function divide() {
+    document.getElementById('inp').value+='÷';
+}
+
+function exp() {
+    document.getElementById('inp').value+='^';
+}
+
+function root() {
+    document.getElementById('inp').value+='√';
+}
+
+function result() {}

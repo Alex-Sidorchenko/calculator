92dfcb5970660dde9c583517c5d2672538305df2
Alex-Sidorchenko <Sidorchenko.alex3@gmail.com>
2018-04-01 01:45:51 +0300

    Using CSS Grid Layout.

--- main.js
+++ main.js
@@ -23,3 +23,24 @@ function root() {
 }
 
 function result() {}
+
+document.getElementsByTagName('input')[0].onkeypress = function(e) {
+    e = e || event;
+    if (e.ctrlKey || e.altKey || e.metaKey) return;
+    var chr = getChar(e);
+    if (chr == null) return;
+    if (chr < '0' || chr > '9') {
+      return false;
+    }
+  }
+  function getChar(event) {
+    if (event.which == null) {
+      if (event.keyCode < 32) return null;
+      return String.fromCharCode(event.keyCode) // IE
+    }
+    if (event.which != 0 && event.charCode != 0) {
+      if (event.which < 32) return null;
+      return String.fromCharCode(event.which) // остальные
+    }
+    return null; // специальная клавиша
+  }

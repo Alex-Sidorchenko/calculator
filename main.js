30884c7d555de6940a9ff372d8865cd4c0635adb
Alex-Sidorchenko <Sidorchenko.alex3@gmail.com>
2018-08-11 15:15:41 +0300

    global changes

--- main.js
+++ main.js
@@ -1,85 +1,85 @@
-btn1.onclick = function() {
-    document.getElementById('inp').value+='+';
-    document.getElementById('inp').focus();
-}
+function Calc() {
+    //При нажатии на кнопки в поле ввода появляются цифры.
+    this.numbers = document.getElementsByClassName("number");
+    for (let i = 0; i < numbers.length; i++) {
+        numbers[i].addEventListener("click", () => {
+            document.getElementById('inp').value += numbers[i].outerText;
+            document.getElementById('inp').focus();
+        });
+    };
 
-btn2.onclick = function() {
-    document.getElementById('inp').value+='-';
-    document.getElementById('inp').focus();
-}
+    //При нажатии на кнопки в поле ввода появляются математические символы.
+    this.symbols = document.getElementsByClassName("symbol");
+    for (let i = 0; i < symbols.length; i++) {
+        symbols[i].addEventListener("click", () => {
+            if (symbols[i].outerText === "x") document.getElementById('inp').value += "*";
+            else if (symbols[i].outerText === "÷") document.getElementById('inp').value += "/";
+            else document.getElementById('inp').value += symbols[i].outerText;
+            document.getElementById('inp').focus();
+        });
+    };
+        
+    //Удаление символа при нажатии на кнопку
+    this.del = del.addEventListener("click", () => {
+        document.getElementById("inp").value = document.getElementById("inp").value.slice(0, -1);
+        document.getElementById('inp').focus();
+    });
 
-btn3.onclick = function() {
-    document.getElementById('inp').value+='*';
-    document.getElementById('inp').focus();
-}
-
-btn4.onclick = function() {
-    document.getElementById('inp').value+='/';
-    document.getElementById('inp').focus();
-}
-
-btn5.onclick = function() {
-    document.getElementById('inp').value+='^';
-    document.getElementById('inp').focus();
-}
-
-btn6.onclick = function() {
-    document.getElementById('inp').value+='√';
-    document.getElementById('inp').focus();
-}
-
-btn7.onclick = function() {
-    //принимает значение, введенное пользователем после нажатия на знак =
-    var result = document.getElementById('inp').value;
-    //проверяет наличие знака ^ в строке
-    if (result.indexOf("^") + 1) {
-        var array = result.split("");
-        var firstnumber = [];
-        var secondnumber = [];
-        // ищет число, которое стояло до знака ^, отбрасывая не цифры, чтобы понять величину аргумента
-        for (var i = result.indexOf("^") - 1; i >= 0; i--) {
-            if (/([0-9.])/.test(array[i])) {
-                firstnumber.unshift(array[i]);
-                continue;
+    //Реализация механизма вычисления выражения после нажатия на клавишу =
+    this.equal = equal.addEventListener("click", () => {
+        //принимает значение, введенное пользователем после нажатия на знак =
+        let result = document.getElementById('inp').value;
+        //проверяет наличие знака ^ в строке
+        if (~result.indexOf("^")) {
+            let array = result.split("");
+            let firstnumber = [];
+            let secondnumber = [];
+            // ищет число, которое стояло до знака ^, отбрасывая не цифры, чтобы понять величину аргумента
+            for (var i = result.indexOf("^") - 1; i >= 0; i--) {
+                if (/([0-9.])/.test(array[i])) {
+                    firstnumber.unshift(array[i]);
+                    continue;
+                }
+                    break;
             }
-            else {
-                break;
+            // ищет число после знака ^, отбрасывая не цифры, чтобы понять величину степени
+            for (var j = result.indexOf("^") + 1; j < array.length; j++) {
+                if (/([0-9.])/.test(array[j])) {
+                    secondnumber.unshift(array[j]);
+                    continue;
+                }
+                else {
+                    break;
+                }
             }
+            //считает сколько символов будет заменено на функцию Math.pow()
+            let deleteCount = 1 + firstnumber.length + secondnumber.length;
+            firstnumber = firstnumber.join("");
+            secondnumber = secondnumber.join("");
+            array.splice(i+1, deleteCount, Math.pow(firstnumber, secondnumber));
+            result = array.join("");
         }
-        // ищет число после знака ^, отбрасывая не цифры, чтобы понять величину степени
-        for (var j = result.indexOf("^") + 1; j < array.length; j++) {
-            if (/([0-9.])/.test(array[j])) {
-                secondnumber.unshift(array[j]);
-                continue;
-            }
-            else {
-                break;
-            }
-        }
-        //считает сколько символов будет заменено на функцию Math.pow()
-        var deleteCount = 1 + firstnumber.length + secondnumber.length;
-        firstnumber = firstnumber.join("");
-        secondnumber = secondnumber.join("");
-        array.splice(i+1, deleteCount, Math.pow(firstnumber, secondnumber));
-        result = array.join("");
-    }
-    // считывает код из строки и выводит его обратно в поле input
-    document.getElementById('inp').value = eval(result);
-    document.getElementById('inp').focus();
-}
+        // считывает код из строки и выводит его обратно в поле input
+        document.getElementById('inp').value = eval(result);
+        document.getElementById('inp').focus();
+    });
 
-inp.onkeyup = function() {
-    //поле input разрешает вводить только определенные символы
-    var regexp = /[^0-9.+()-/*^]/;
-    if (regexp.test(document.getElementById("inp").value)) {
-        var newarray = document.getElementById("inp").value.split("");
-        newarray.length -= 1;
-        document.getElementById("inp").value = newarray.join("");
-    }
-    //заменяет запятую на точку
-    else if (/,/.test(document.getElementById("inp").value)) {
-        var newarray = document.getElementById("inp").value.split("");
-        newarray.splice(newarray.length-1, 1, ".");
-        document.getElementById("inp").value = newarray.join("");
-    }
-}
+    //Поле input разрешает вводить только цифры и арифметические символы
+    inp.onkeyup = function() {
+        //увидев равно, начинает считать
+        if (/[^0-9.+()-=/*√^]/.test(document.getElementById("inp").value)) {
+            let newarray = document.getElementById("inp").value.split("");
+            newarray.length -= 1;
+            document.getElementById("inp").value = newarray.join("");
+        }
+        //заменяет запятую на точку
+        else if (/,/.test(document.getElementById("inp").value)) {
+            let newarray = document.getElementById("inp").value.split("");
+            newarray.splice(newarray.length-1, 1, ".");
+            document.getElementById("inp").value = newarray.join("");
+        }
+    };
+    
+};
+
+let calc = Calc ();
